6dd6c78595d387bf94df64900de0e2a68fa35f22
kyerbolatovna24-creator <k.yerbolatovna24@apec.edu.kz>
2026-02-06 16:03:47 +0500

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,24 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Choose operation (+ - * /): ")
+if operation == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operation == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operation == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operation == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        result = num1 / num2
+        print("Result:", result)
+
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
